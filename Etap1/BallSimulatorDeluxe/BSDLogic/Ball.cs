1b52a5efd2caaa3153fb8b60ee35d72859c239be
Amadeusz Sitnicki <amadeusz.sitnicki1@gmail.com>
2023-04-15 00:52:16 +0200

    Ball class definition

--- Etap1/BallSimulatorDeluxe/BSDLogic/Ball.cs
+++ Etap1/BallSimulatorDeluxe/BSDLogic/Ball.cs
@@ -1,7 +1,36 @@
-﻿namespace BSDLogic
+﻿using System.Drawing;
+using System.Numerics;
+using System.Windows;
+
+namespace BSDLogic
 {
     class Ball
     {
+        private Point location;
+        private double radius;
+        private Color color;
+        private Vector2 velocity;
+       
+        public Ball(Point location, double radius, Color color, Vector2? velocity = null)
+        {
+            this.location = location;
+            this.radius = radius;
+            this.color = color;
+            this.velocity = velocity ?? new Vector2(0);
+        }
+        
+        public Point Location
+        {
+            get => this.location;
+            set => this.location = value;
+        }
+        public double Radius => this.radius;
+        public Color Color => this.color;
+        public Vector2 Velocity
+        {
+            get => this.velocity;
+            set => this.velocity = value;
+        }
 
     }
 }
