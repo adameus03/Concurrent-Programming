4fc56a429e5424a0cda1d1d180fe332bfa4cfd83
Amadeusz Sitnicki <amadeusz.sitnicki1@gmail.com>
2023-04-16 22:28:46 +0200

    Model-logic layer interfacing

--- Etap1/BallSimulatorDeluxe/BSDMVVM/BSDModel.cs
+++ Etap1/BallSimulatorDeluxe/BSDMVVM/BSDModel.cs
@@ -1,6 +1,7 @@
 ﻿using BSDLogic;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,9 +11,39 @@ namespace BSDMVVM
     class BSDModel
     {
         private BSDLogic.BSDAbstractLogicAPI logicAPI;
+        private int simulationNumberOfBalls;
+        
+        private int simulationFPS = 60;
+        private int simulationJumpingFactor = 1;
+        private Color colorForEachBall = Color.AliceBlue;
         public BSDModel(BSDLogic.BSDAbstractLogicAPI? logicAPI = null)
         {
             this.logicAPI = logicAPI ?? BSDLogic.BSDAbstractLogicAPI.CreateInstance();
         }
+        public void EllipsesInitialization()
+        {
+            this.logicAPI.GenerateBalls(this.simulationNumberOfBalls, this.colorForEachBall);
+        }
+
+        public void EllipsesRepositioning()
+        {
+            this.logicAPI.UpdateBalls(1000 / this.simulationFPS, this.simulationJumpingFactor);
+        }
+
+        public int NumberOfBalls { get => this.simulationNumberOfBalls; set => this.simulationNumberOfBalls = value; }
+        public Color ColorForEachBall { get => this.colorForEachBall; set => this.colorForEachBall = value; }
+
+        public int SimulationFPS { 
+            get => this.simulationFPS;
+            set
+            {
+                if (value > 1000)
+                {
+                    this.simulationFPS = 1000;
+                }
+                else this.simulationFPS = value;
+            }
+        }
+        public int SimulationJumpingFactor { get => this.simulationJumpingFactor; set => this.simulationJumpingFactor = value; }
     }
 }
