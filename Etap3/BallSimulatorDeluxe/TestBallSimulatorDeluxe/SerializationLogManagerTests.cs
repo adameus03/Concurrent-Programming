cfff1493378bf6d3dc8b2228ed596482b2daeee7
Artosz <75026538+Artosz@users.noreply.github.com>
2023-06-05 09:44:36 +0200

    Additional tests

--- Etap3/BallSimulatorDeluxe/TestBallSimulatorDeluxe/SerializationLogManagerTests.cs
+++ Etap3/BallSimulatorDeluxe/TestBallSimulatorDeluxe/SerializationLogManagerTests.cs
@@ -10,17 +10,32 @@ namespace TestBallSimulatorDeluxe
 {
     internal class SerializationLogManagerTests
     {
-        SerializationLogManager serializationLogManager;
+        SerializationLogManager serializationLogManager1;
+        SerializationLogManager serializationLogManager2;
+        string testFilePath = "test.log";
         [SetUp]
         public void Setup()
         {
-            serializationLogManager = SerializationLogManager.CreateInstance();
+            serializationLogManager1 = SerializationLogManager.CreateInstance();
+            serializationLogManager2 = SerializationLogManager.CreateInstance(testFilePath);
         }
 
         [Test]
         public void Test1()
         {
-            Assert.IsInstanceOf(typeof(SerializationLogManager), serializationLogManager);
+            Assert.IsInstanceOf(typeof(SerializationLogManager), serializationLogManager1);
+            Assert.IsNull(serializationLogManager1.FilePath);
+            Assert.IsFalse(serializationLogManager1.IsActive);
+            Assert.IsFalse(serializationLogManager1.IsLogging);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.That(serializationLogManager2.FilePath, Is.EqualTo(testFilePath));
+            Assert.IsFalse(serializationLogManager1.IsActive);
+            Assert.IsFalse(serializationLogManager1.IsLogging);
+
         }
     }
 }
